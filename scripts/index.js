c5046f857803a6e0428e55ef1c8f65e46d5dea5c
Chedlyrdissi <chedlyrd2000@gmail.com>
2020-06-14 23:52:41 -0400

    still a bit more to go

--- scripts/index.js
+++ scripts/index.js
@@ -119,7 +119,24 @@ $(document).ready(function() {
 	 $("#hairdresserNext").attr('disabled', true);
 	 $("#paymentNext").attr('disabled', true);
 	 $("#finalBreadBtn").attr('disabled', true);
+	 $("#dateNext").attr("disabled", true);
 	 
+
+	 $("#hairdresserNext").click(function(){
+	 	const hairdresser = getHairdresser();
+	 	$("#chosenHairdresser").html(hairdresser);
+	 	
+	 	let av;
+	 	if (hairdresser == "no preferences") {
+			av = "monday - saturday";
+    	} else if (hairdresser == "Julius Caesar") {
+    		av = "monday, wednesday, friday";
+    	} else if (hairdresser == "Anna Green") {
+			av = "tuesday, thirsday, saturday";
+    	}
+
+    	$("#avDays").html(av);
+	 });
 
 	$("#startBookingBtn").click(function() {
 		this.style.display = "none";
@@ -160,6 +177,111 @@ $(document).ready(function() {
         mask: cvvn, 
         greedy: false, 
         definitions: { '#': { validator: "[0-9]", cardinality: 1}} });
-    // $('#cvvNum').inputmask("999");
 
+    $("#dateTimeInput").change(function(){
+
+    	$("#dateMsgSuccess").hide();
+	    $("#dateMsgDangerFormat").hide();
+	    $("#dateMsgDangerAv").hide();
+
+	    let dateTime = {};
+
+    	dateTime.year = this.value.slice(0,4);
+    	dateTime.month = this.value.slice(5,7);
+    	dateTime.day = this.value.slice(8,10);
+    	dateTime.hour = this.value.slice(12,14);
+    	dateTime.minute = this.value.slice(15,17);
+    	
+    	let choice = getHairdresser();
+    	let availableDays;
+    	if (choice == "no preferences") {
+			availableDays = [0,1,2,3,4,5];
+    	} else if (choice == "Julius Caesar") {
+    		availableDays = [0,2,4];
+    	} else if (choice == "Anna Green") {
+			availableDays = [1,3,5];
+    	}
+
+    	const invalidFormat = "Date format is invalid, Please follow the format requested";
+    	const invalidDay = "The day you have chosen doesn't work for us";
+
+    	let invalids = [];
+    	
+		if (dateTime.year === undefined) {
+			if (invalids.indexOf(invalidFormat) < 0) {
+				invalids.push(invalidFormat);
+			}
+		}	
+    	
+		if (dateTime.month === undefined) {
+			if (invalids.indexOf(invalidFormat) < 0) {
+				invalids.push(invalidFormat);
+			}
+		}
+
+		if (dateTime.day === undefined ||
+			dateTime.day > 31 ||
+			dateTime.day < 0) {
+			if (invalids.indexOf(invalidFormat) < 0) {
+				invalids.push(invalidFormat);
+			}
+		} else {
+			let day = (new Date(dateTime.year +"-"+ dateTime.month +"-"+ dateTime.day)).getDay();
+			
+			if ( availableDays.indexOf(day) < 0 ) {
+				if (invalids.indexOf(invalidDay) < 0) {
+    				invalids.push(invalidDay);
+    			}
+			}
+		}
+    	
+		if (dateTime.hour === undefined ||
+			dateTime.hour < 0 ||
+			dateTime.hour > 23) {
+			if (invalids.indexOf(invalidFormat) < 0) {
+				invalids.push(invalidFormat);
+			}
+		}
+    	
+		if (dateTime.minute === undefined || 
+			dateTime.minute > 59 || 
+			dateTime.minute < 0) {
+			if (invalids.indexOf(invalidFormat) < 0) {
+				invalids.push(invalidFormat);
+			}
+		}
+
+
+       	if (invalids.length === 0) {
+       		$("#dateMsgSuccess").show();
+       		$("#dateNext").attr("disabled", false);
+
+       	} else {
+       		if (invalids.indexOf(invalidFormat) >= 0) {
+       			$("#dateMsgDangerFormat").show();
+       		}
+
+       		if (invalids.indexOf(invalidDay) >= 0) {
+				$("#dateMsgDangerAv").show();
+       		}
+       		$("#dateNext").attr("disabled", true);
+       	}
+
+    });
+
+    $("#dateMsgSuccess").hide();
+    $("#dateMsgDangerFormat").hide();
+    $("#dateMsgDangerAv").hide();
 });
+
+function getHairdresser() {
+	let choice;
+	let list = $("[name='hairdresserChoice']");
+	for(let i=0; i<list.length; i++) {
+		if (list[i].checked) {
+   			choice = list[i].value;
+			break;
+		}
+	}
+	return choice;
+}
